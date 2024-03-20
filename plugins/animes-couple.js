b15c162a2962599ff116d847be60fc20b4f355ce
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-03-20 15:26:55 +0000

    Update animes-couple.js

--- plugins/animes-couple.js
+++ plugins/animes-couple.js
@@ -1,13 +1,13 @@
 import fetch from "node-fetch"
 let handler = async (m, { conn }) => {
 
-  let data = await (await fetch('اوتشيها')).json()
+  let data = await (await fetch('')).json()
   let cita = data[Math.floor(Math.random() * data.length)]
   
   let cowi = await(await fetch(cita.cowo)).buffer()
-  await conn.sendFile(m.chat, cowi, '', '_أرسلها لصديقتك♂️_ \n  \ديابلو', m)
+  await conn.sendFile(m.chat, cowi, '', '_أرسلها لصديقتك♂️_ \n \', m)
   let ciwi = await(await fetch(cita.cewe)).buffer()
-  await conn.sendFile(m.chat, ciwi, '', '_ارسلها لصديقك♀️_\n ولا تنسى متابعتي هنا \ninstagram.com/noureddine_ouafy', m)
+  await conn.sendFile(m.chat, ciwi, '', '_ارسلها لصديقك♀️_\n \', m)
 }
 handler.help = ['ppcouple','tofanime']
 handler.tags = ['anime']
