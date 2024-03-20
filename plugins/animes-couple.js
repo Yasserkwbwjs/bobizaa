ebf05f4aca9062d8c0a515227ec3c80c86d5d6e7
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-03-20 15:16:16 +0000

    Update animes-couple.js

--- plugins/animes-couple.js
+++ plugins/animes-couple.js
@@ -1,11 +1,11 @@
 import fetch from "node-fetch"
 let handler = async (m, { conn }) => {
 
-  let data = await (await fetch('https://raw.githubusercontent.com/KazukoGans/database/main/anime/ppcouple.json')).json()
+  let data = await (await fetch('اوتشيها')).json()
   let cita = data[Math.floor(Math.random() * data.length)]
   
   let cowi = await(await fetch(cita.cowo)).buffer()
-  await conn.sendFile(m.chat, cowi, '', '_أرسلها لصديقتك♂️_ \nولا تنسى متابعتي هنا \ninstagram.com/noureddine_ouafy', m)
+  await conn.sendFile(m.chat, cowi, '', '_أرسلها لصديقتك♂️_ \n  \ديابلو', m)
   let ciwi = await(await fetch(cita.cewe)).buffer()
   await conn.sendFile(m.chat, ciwi, '', '_ارسلها لصديقك♀️_\n ولا تنسى متابعتي هنا \ninstagram.com/noureddine_ouafy', m)
 }
