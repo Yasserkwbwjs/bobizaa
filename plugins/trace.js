7da10c86d2cc39cf0b26a8cd6f6e0daa4e157f64
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-03-25 21:20:52 +0000

    Update trace.js

--- plugins/trace.js
+++ plugins/trace.js
@@ -66,8 +66,8 @@ function formatDuration(ms) {
   return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 }
 
-handler.help = ["trace"];
+handler.help = ["من-اين"];
 handler.tags = ["anime"];
-handler.command = /^trace$/i;
+handler.command = /^من-اين$/i;
 
 export default handler;
