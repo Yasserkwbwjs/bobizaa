ae1430a12efd9def2e6d3e474977d89ce27d87fd
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-03-25 02:57:15 +0000

    Update clock.js

--- plugins/clock.js
+++ plugins/clock.js
@@ -13,8 +13,8 @@ let wib = moment.tz('Africa/casablanca').format('HH:mm:ss')
   conn.reply(m.chat, clockString, m);
 }
 
-handler.help = ['clock'];
+handler.help = ['الوقت'];
 handler.tags = ['tools'];
-handler.command = /^(clock)$/i;
+handler.command = /^(الوقت)$/i;
 
 export default handler;
