10d1bf4d5c3425594dfdb51deb582e7e77735c58
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-03-25 02:32:03 +0000

    Update bader-lmchari.js

--- plugins/bader-lmchari.js
+++ plugins/bader-lmchari.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 			await conn.sendMessage(m.chat, { video: { url: dir[Math.floor(Math.random() * dir.length)] }, caption: `_تابع صاحب البوت في الانستغرام_\ninstagram.com/noureddine_ouafy` }, { quoted: m })
 	}
 
-handler.help = ['bader']
+handler.help = ['شيخ-بدر']
 handler.tags = ['islam']
-handler.command = /^(bader)$/i
+handler.command = /^(شيخ-بدر)$/i
 handler.limit = false
 
 export default handler
