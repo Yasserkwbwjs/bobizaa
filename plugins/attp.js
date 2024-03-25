3890606c507e3ebad9cdbe2465c3cf963f9c4102
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-03-25 02:10:48 +0000

    Update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -5,7 +5,7 @@ let handler = async(m, { conn, text, args, usedPrefix, command }) => {
 if (!text) throw `*تحويل كلمة او نص لملصق*\n\n*—◉ مثال:*\n*◉ ${usedPrefix + command} Uchiha-Diablo*`
 let teks = encodeURI(text)
 
-if (command == 'attp') {
+if (command == 'نص-ملصق') {
 let a1 = await (await fetch(`https://api.erdwpe.com/api/maker/attp?text=${teks}`)).buffer()
 let a2 = await createSticker(a1, false, global.packname, global.author)
 conn.sendFile(m.chat, a2, 'sticker.webp', '', m, { asSticker: true })}
