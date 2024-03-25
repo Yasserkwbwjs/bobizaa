a6c1e61406774c8c5f92d336dfb5402b4a893250
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-03-25 10:36:37 +0000

    Update deidara.js

--- plugins/deidara.js
+++ plugins/deidara.js
@@ -4,11 +4,11 @@ const handler = async (m, usedPrefix, command) => {
   const g = await fetch(`https://raw.githubusercontent.com/inirey/RESTAPI/master/data/deidara.json`);
   const f = await g.json();
   const a = f[Math.floor(Math.random() * f.length)];
-  conn.sendFile(m.chat, a, "deidara.jpg", 'instagram.com/noureddine_ouafy', m);
+  conn.sendFile(m.chat, a, "deidara.jpg", '', m);
 };
 
-handler.help = ["deidara"];
+handler.help = ["ديدارا"];
 handler.tags = ["anime"];
-handler.command = /^deidara$/i;
+handler.command = /^ديدارا$/i;
 
 export default handler;
