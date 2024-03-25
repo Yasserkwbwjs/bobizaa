5dc760b9b681523f25a8f44600c1544b6c702e3d
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-03-25 02:37:42 +0000

    Update ban.js

--- plugins/ban.js
+++ plugins/ban.js
@@ -1,12 +1,12 @@
 let handler = async (m, { participants }) => {
     // if (participants.map(v=>v.jid).includes(global.conn.user.jid)) {
     global.db.data.chats[m.chat].isBanned = true
-    m.reply('تم حظرك من استعمال البوت يا عزيزي لانك تخالف سياسة استعماله ♥ يمكنك معرفة سياسة استخدام البوت لدى صاحبه نورالدين \ninstagram.com/noureddine_ouafy 😄!')
+    m.reply('تم حظرك من استعمال البوت يا عزيزي لانك تخالف سياسة استعماله ♥ يمكنك معرفة سياسة استخدام البوت لدى صاحبه اوتشيها ديابلو \ninstagram.com/noureddine_ouafy 😄!')
     // } else m.reply('Ada nomor host disini...')
 }
-handler.help = ['ban']
+handler.help = ['باي']
 handler.tags = ['owner']
-handler.command = /^ban$/i
+handler.command = /^باي$/i
 
 handler.owner = true
 
