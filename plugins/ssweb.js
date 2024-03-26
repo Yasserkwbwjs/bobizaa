c5b3f95681c2eb42ddcb2bd02bfc494bdae8f8ff
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-03-26 00:10:24 +0000

    Update ssweb.js

--- plugins/ssweb.js
+++ plugins/ssweb.js
@@ -19,9 +19,9 @@ if (command === 'sspc') {
 await conn.sendFile(m.chat, desktop.result, '',res, m, false)
 }
 }
-handler.help = ['ssweb']
+handler.help = ['سكرين']
 handler.tags = ['tools']
-handler.command = /^(ssweb)$/i
+handler.command = /^(سكرين)$/i
 
 handler.limit = false
 
