a923e83a2db1a2af2b8a01e67b627742f145272c
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-03-28 14:38:26 +0000

    Update animes-couple.js

--- plugins/animes-couple.js
+++ plugins/animes-couple.js
@@ -1,17 +1,17 @@
 import fetch from "node-fetch"
 let handler = async (m, { conn }) => {
 
-  let data = await (await fetch('')).json()
+  let data = await (await fetch('https://raw.githubusercontent.com/KazukoGans/database/main/anime/ppcouple.json')).json()
   let cita = data[Math.floor(Math.random() * data.length)]
   
   let cowi = await(await fetch(cita.cowo)).buffer()
-  await conn.sendFile(m.chat, cowi, '', '_أرسلها لصديقتك♂️_ \nولا تنسى متابعتي هنا \ninstagram.com/noureddine_ouafy', m)
+  await conn.sendFile(m.chat, cowi, '', '_أرسلها لصديقتك♂️_ \nولا تنسى متابعتي هنا \', m)
   let ciwi = await(await fetch(cita.cewe)).buffer()
-  await conn.sendFile(m.chat, ciwi, '', '_ارسلها لصديقك♀️_\n ولا تنسى متابعتي هنا \ninstagram.com/noureddine_ouafy', m)
+  await conn.sendFile(m.chat, ciwi, '', '_ارسلها لصديقك♀️_\n ولا تنسى متابعتي هنا \', m)
 }
-handler.help = ['تطقيم','تطقيم']
+handler.help = ['تطقيم1','تطقيم']
 handler.tags = ['anime']
-handler.command = ['تطقيم','تطقيم'] 
+handler.command = ['تطقيم1','تطقيم'] 
 
 
 export default handler
