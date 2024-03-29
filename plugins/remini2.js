c552a9d5092bb2a6d4b57dff38b239500fca6a75
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-03-29 16:13:22 +0000

    Update remini2.js

--- plugins/remini2.js
+++ plugins/remini2.js
@@ -11,7 +11,7 @@ async function handler(m, { conn, usedPrefix, command }) {
             const api = await fetch(`https://api.betabotz.eu.org/api/tools/remini?url=${out}&apikey=beta-Nourr123`);
             const image = await api.json();
             const { url } = image;
-            conn.sendFile(m.chat, url, null, 'instagram.com/noureddine_ouafy', m);
+            conn.sendFile(m.chat, url, null, '🌷*النتيجة*', m);
         } else {
             m.reply(`هذا الامر خاص بزيادة جودة اي صورة يكفي فقط ان ترسل للبوت الصورة وتقوم بالاشارة اليها وتكتب :\n*${usedPrefix + command}*`);
         }
@@ -21,8 +21,8 @@ async function handler(m, { conn, usedPrefix, command }) {
     }
 }
 
-handler.help = ['remini2'];
+handler.help = ['*لجود*'];
 handler.tags = ['image-edit'];
-handler.command = ['remini2'];
+handler.command = ['لجود'];
 
 export default handler;
