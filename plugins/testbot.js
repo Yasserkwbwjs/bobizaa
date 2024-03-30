5e5b685deddc7cb630489fce2474e396c588e6e8
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-03-30 02:01:51 +0000

    Update testbot.js

--- plugins/testbot.js
+++ plugins/testbot.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `بوبيزة متصلة الان بالانترنيت \nيمكنك كتابة \n.menu\n لتنبتق لك جميع الاوامر \n@${m.sender.split('@')[0]} \n`
+let info = ` مرحبا بك كايف يمكنني مساعدتك 🌹\nيمكنك كتابة \n.\n *🙂ان لست بوت اسمي رينجو* \n@${m.sender.split('@')[0]} \n`
 await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forwardingScore: 65,
       isForwarded: true, externalAdReply: { title: author, body: bottime,thumbnail: fs.readFileSync('./thumbnail.jpg') }}})
       await conn.sendMessage(m.chat, {
@@ -17,7 +17,7 @@ await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forw
         quoted: m
     })
 }
-handler.customPrefix = /^(tes|سلام|menu|Menu|apk|bobizaty|♥|salam|hy|Hello|.|شكرا|مرحبا)$/i
+handler.customPrefix = /^(بوت)$/i
 handler.command = new RegExp
 
 export default handler
