4c77a3b630a2bf4900e71e1c83e7a7ed42719f0a
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-03-30 16:15:05 +0000

    Update لوغو2.js

--- plugins/لوغو2.js
+++ plugins/لوغو2.js
@@ -12,8 +12,8 @@ const handler = async (m, { conn, args, text, command, usedPrefix, isCreator, is
   let res = `[0]}`;
   conn.sendFile(m.chat, res, 'idulfitri.jpg', 'instagram.com/noureddine_ouafy', m, false);
 };
-handler.help = ['تهنئه'];
+handler.help = ['*لوغو2*'];
 handler.tags = ['logo'];
-handler.command = /^(تهنئه)$/i;
+handler.command = /^(لوغو2)$/i;
 
 export default handler;
