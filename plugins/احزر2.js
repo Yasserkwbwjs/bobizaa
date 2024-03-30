d3b0ff0d2ce6e91a019c7b04a94a3b35a212ee11
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-03-30 17:29:22 +0000

    Update احزر2.js

--- plugins/احزر2.js
+++ plugins/احزر2.js
@@ -6,13 +6,13 @@ export async function before(m) {
         return !0
     this.tebakbendera = this.tebakbendera ? this.tebakbendera : {}
     if (!(id in this.tebakbendera))
-        return this.reply(m.chat, '*❐┃لقد انتها هذا الامر اكتبه مجددا لي العب من جديد┃🌸 ❯*\n*『𝑅𝐼𝑇𝐴🌸𝐵𝛩𝑇』*', m)
+        return this.reply(m.chat, '*❐┃لقد انتها هذا الامر اكتبه مجددا لي العب من جديد┃🧊 ❯*\n*『𝑅𝐼𝑇𝐴🌸𝐵𝛩𝑇』*', m)
     if (m.quoted.id == this.tebakbendera[id][0].id) {
         let isSurrender = /^(انسحب|surr?ender)$/i.test(m.text)
         if (isSurrender) {
             clearTimeout(this.tebakbendera[id][3])
             delete this.tebakbendera[id]
-            return this.reply(m.chat, '*❐┃لم تكن كما ضننت┃🌸 ❯*\n*『𝑅𝐼𝑇𝐴🌸𝐵𝛩𝑇』*', m)
+            return this.reply(m.chat, '*❐┃لم تكن كما ضننت┃🧊 ❯*\n*『亗 ᴹᴿ ꪜ RINGO☂』*', m)
         }
         let json = JSON.parse(JSON.stringify(this.tebakbendera[id][1]))
 
@@ -22,9 +22,9 @@ export async function before(m) {
             clearTimeout(this.tebakbendera[id][3])
             delete this.tebakbendera[id]
         } else if (similarity(m.text.toLowerCase(), json.name.toLowerCase().trim()) >= threshold)
-            m.reply(`*❐┃لقد كنت على وشك النجاح┃❌ ❯*\n*『𝑅𝐼𝑇𝐴🌸𝐵𝛩𝑇』*`)
+            m.reply(`*❐┃لقد كنت على وشك النجاح┃❌ ❯*\n*『亗 ᴹᴿ ꪜ RINGO☂』*`)
         else
-            this.reply(m.chat, `*❐┃اجـابـة خـاطـئـة┃❌ ❯*\n*『𝑅𝐼𝑇𝐴🌸𝐵𝛩𝑇』*`, m)
+            this.reply(m.chat, `*❐┃اجـابـة خـاطـئـة┃❌ ❯*\n*『亗 ᴹᴿ ꪜ RINGO☂』*`, m)
     }
     return !0
 }
