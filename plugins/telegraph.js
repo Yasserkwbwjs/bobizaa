7e6a9cdc681dc25151efbd8b068140051dfa6575
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-03-30 01:52:16 +0000

    Update telegraph.js

--- plugins/telegraph.js
+++ plugins/telegraph.js
@@ -4,7 +4,7 @@ import uploadImage from '../lib/uploadImage.js'
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'هذا الامر خاص برفع الصور لموقع\nhttps://telegra.ph/\nالان قم بالاشارة للصورة التي تريد رفعها للموقع وسوف تحصل على رابطها \n اشر اليها ثم اكتب\n*.telegraph*'
+  if (!mime) throw 'هذا الامر خاص برفع الصور لموقع\nhttps://telegra.ph/\nالان قم بالاشارة للصورة التي تريد رفعها للموقع وسوف تحصل على رابطها \n اشر اليها ثم اكتب\n*.تليجراف*'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
