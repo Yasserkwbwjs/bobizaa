4a002465cb8e6da83a49214c500b38b1c195f5f7
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-04-02 14:08:32 +0000

    Update 6.js

--- plugins/6.js
+++ plugins/6.js
@@ -57,7 +57,7 @@ conn.sendMessage(m.chat, {
 }; 
 handler.help = ['main']
 handler.tags = ['group']
-handler.command = ['3'] 
+handler.command = ['6'] 
 
 export default handler
 function clockString(ms) {
