ab628b38811fc8ca977cae5254583478bf5e4cb7
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-04-02 12:18:48 +0000

    Update 1.js

--- plugins/1.js
+++ plugins/1.js
@@ -9,7 +9,13 @@ import { join } from 'path'
 const time = moment.tz('Egypt').format('HH')
 let wib = moment.tz('Egypt').format('HH:mm:ss')
 //import db from '../lib/database.js'
-
+let handler = async (m, { conn, usedPrefix, command }) => {
+  await conn.sendMessage(m.chat, {
+   react: {
+ text: "🖤",
+ key: m.key,
+   }
+  })
 let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
     let d = new Date(new Date + 3600000)
     let locale = 'ar'
