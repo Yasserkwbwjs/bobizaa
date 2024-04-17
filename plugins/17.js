12d51729453e5538761f5ce2b5f92ae2a83b3a9d
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-04-17 23:06:02 +0100

    Update 17.js

--- plugins/17.js
+++ plugins/17.js
@@ -1,34 +1,12 @@
-//https://whatsapp.com/channel/0029VaWuSkHGehEKy41ibw3k
-
-let handler = m => m
-handler.all = async function (m) {
-
-    if (m.messageStubType == 9 && m.message) { // Check if it's a sticker message
-        let stickerId = m.message.stickerMessage.fileSha256.toString('base64');
-        let stickerUrl = `https://stickershop.line-scdn.net/stickershop/v1/sticker/${stickerId}/android/sticker.png`;
-        this.sendFile(m.chat, stickerUrl, 'sticker.png', '🤖', m);
-        return true; // Return true to indicate message is handled
-    }
-
-    if (/^بيض$/i.test(m.text) ) {
-      let av = 'رابط الصوره'
-      this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-      return true; // Return true to indicate message is handled
-    }
-
-    if (/^احبك$/i.test(m.text) ) {
-       let av = 'رابط الصوره'
-       this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-       return true; // Return true to indicate message is handled
-    }
-
-    if (/^اسطوري$/i.test(m.text) ) {
-        let av = 'رابط الصوره'
-        this.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
-        return true; // Return true to indicate message is handled
-    }
-  
-    return false; // Return false if message is not handled
-}
- 
+const handler = async (m, {conn, text}) => {
+  if (!text) throw 'ضيف نص يحب👾';
+  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/youtube-comment', {
+    avatar: await conn.profilePictureUrl(m.sender, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+    comment: text,
+    username: conn.getName(m.sender),
+  }), 'error.png', '*❆╼━━❆ •﹝*『🍷┇𝐒𝐔𝐍𝐆 𝐁𝐎𝐓*﹞• ❆━━╾❆*', m);
+};
+handler.help = ['ytcomment <comment>'];
+handler.tags = ['maker'];
+handler.command = /^(تعليق)$/i;
 export default handler;
