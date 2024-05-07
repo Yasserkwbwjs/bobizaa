0b633dd5e10bede34590daa8cc1e53988f290582
Bonjour12753 <162489510+Bonjour12753@users.noreply.github.com>
2024-05-07 02:17:30 +0100

    Update 10.js

--- plugins/10.js
+++ plugins/10.js
@@ -19,7 +19,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
     let uptime = clockString(_uptime)
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let videoUrl = 'https://telegra.ph/file/4301008c2efdad4a15604.jpg';
+let videoUrl = 'https://telegra.ph/file/4383e92996a2787aede0d.mp4';
   let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
   const {money, joincount} = global.db.data.users[m.sender];
